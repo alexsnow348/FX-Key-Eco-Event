e8f3b1c15c2f65a4d51398b34b1df43e67215523
Alex Snow <alexsnow348@gmail.com>
2018-10-31 08:45:02 -0400

    filter impact 3

--- main.py
+++ main.py
@@ -16,16 +16,24 @@ class API(object):
         self.data_df = pd.DataFrame(data)
         self.data_df = self.data_df.dropna(how='any', axis=0)
 
-    def processData(self):
+    def processData(self, date):
 
         self.data_df['timestamp_af'] = self.data_df['timestamp'].apply(
-            lambda x: int(time.mktime(time.strptime(x, '%Y-%m-%d %H:%M:%S'))) - time.timezone)
+            lambda x: int(time.mktime(time.strptime(x, '%Y-%m-%d %H:%M:%S')))
+            - time.timezone)
         self.data_df['timestamp_af'] = self.data_df['timestamp_af'].apply(
             lambda x: time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(x)))
-        print(self.data_df[['timestamp', 'timestamp_af']])
+        self.data_df['date'] = self.data_df['timestamp_af'].apply(
+            lambda x: x[:10])
+        todaydata = self.data_df.loc[self.data_df['date'] == date]
+        impactful_data = todaydata.loc[todaydata['impact'] == 3]
+        print(impactful_data)
 
 
 if __name__ == '__main__':
     url = 'https://eco-event.000webhostapp.com/'
+    date = datetime.now()
+    date = date.strftime("%Y-%m-%d")
+    print(date)
     sample_call = API(url)
-    sample_call.processData()
+    sample_call.processData(date)
