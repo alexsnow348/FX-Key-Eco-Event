9345eb7db13a392323bece9207aba21729eb1367
Wut Hmone Hnin Hlaing <wut.hmone@tmrnd.com.my>
2018-11-12 17:20:08 +0800

    twillo function added

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
+"""Key Economic Event Notification Engine.
+
+The engine will notify the impacful infomation to the required agent.
+
+Example:
+        $ python main.py
+
+Todo:
+    * Get level 3 impactful data (done)
+    * Change timezone to locals (done)
+    * Get list of time and change it into 3 minutes before timestamp (done)
+    * Send twillo message to phone when current time is in the time list.
+
+"""
+
 import requests as rq
 import pandas as pd
 import json
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
+from twilio.rest import Client
 
 
 class API(object):
@@ -20,21 +36,79 @@ class API(object):
         local = utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
         return local.strftime("%Y-%m-%d %H:%M:%S")
 
-    def processData(self, date):
+    def three_min_behind(self, timestring):
+        time_dt = datetime.strptime(timestring, "%Y-%m-%d %H:%M:%S")
+        time_dt_3_min = time_dt - timedelta(minutes=3)
+        time_dt_3_min = time_dt_3_min.strftime("%s")
+        return time_dt_3_min
 
-        self.data_df['timestamp_af'] = self.data_df['timestamp'].apply(
+    def change_24_to_12(self, timestring):
+        time_dt = datetime.strptime(timestring, "%Y-%m-%d %H:%M:%S")
+        time_dt = time_dt.strftime("%I:%M:%S %p")
+        return time_dt
+
+    def send_msg(self, msg):
+        # Your Account SID from twilio.com/console
+        account_sid = "AC0dadb0ce3f1db887ecf2cc5209932676"
+        # Your Auth Token from twilio.com/console
+        auth_token = "7eb9f55beeb3cfea278ccc183cf0f46a"
+
+        client = Client(account_sid, auth_token)
+
+        message = client.messages.create(
+            to="+601128174379",
+            from_="+17067395816",
+            body=msg)
+        return message.sid
+
+    def processData(self, date, major):
+
+        impactful_data = self.data_df.loc[self.data_df['impact'] == 3].copy()
+        impactful_data = self.data_df
+        impactful_data['timestamp_af'] = impactful_data['timestamp'].apply(
             lambda x: self.utc_to_local(x))
-        self.data_df['date'] = self.data_df['timestamp_af'].apply(
+        impactful_data['date'] = impactful_data['timestamp_af'].apply(
             lambda x: x[:10])
-        todaydata = self.data_df.loc[self.data_df['date'] == date]
-        impactful_data = todaydata.loc[todaydata['impact'] == 3]
-        print(impactful_data)
+        impactful_data = impactful_data.loc[
+            impactful_data['date'] == date].copy()
+        impactful_data['tmsp_3_min'] = impactful_data['timestamp_af'].apply(
+            lambda x: self.three_min_behind(x))
+        impactful_data['major'] = impactful_data['economy'].apply(
+            lambda x: True if x in major else False)
+        impactful_data = impactful_data.loc[
+            impactful_data['major'] == True].copy()
+        impactful_data['timestamp_af'] = impactful_data['timestamp_af'].apply(
+            lambda x: self.change_24_to_12(x))
+        self.final_data = impactful_data[['economy', 'name',
+                                          'impact', 'timestamp_af',
+                                          'tmsp_3_min']].copy()
+        self.time_list = list(impactful_data['tmsp_3_min'])
+
+    def create_msg(self, time_to_send):
+        if time_to_send in self.time_list:
+            data = self.final_data.loc[
+                self.final_data['tmsp_3_min'] == time_to_send].copy()
+            data = json.loads(data.to_json(orient='records'))
+            main_body = str()
+            for each in data:
+                body = "Based: "+each['economy']+"\n"\
+                    + "Info: "+each['name']+"\n"\
+                    + "Impact: "+str(each['impact'])+"\n"\
+                    + "Time: "+each['timestamp_af']
+                main_body = main_body + "\n"+body
+            code = self.send_msg(msg=body)
+            print(code)
 
 
 if __name__ == '__main__':
+    major = ['EUR', 'USD', 'JPY', 'GBP', 'AUD', 'CHF', 'NZD', 'CAD']
     url = 'https://eco-event.000webhostapp.com/'
-    date = datetime.now()
-    date = date.strftime("%Y-%m-%d")
+    date_time = datetime.now()
+    date = date_time.strftime("%Y-%m-%d")
     print(date)
+    date_time = date_time - timedelta(minutes=3)
+    date_time = date_time.strftime("%s")
+    print(date_time)
     sample_call = API(url)
-    sample_call.processData(date)
+    sample_call.processData(date, major)
+    sample_call.create_msg(date_time)
--- scrap_data.py
+++
@@ -1,9 +0,0 @@
-import requests as rq
-import pandas as pd
-import json
-from datetime import datetime
-
-url = 'https://sslecal2.forexprostools.com/'
-
-request = rq.get(url)
-print(request.text)
